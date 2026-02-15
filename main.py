52a42911d3029a904687cc05efc6ef5d922cab56
Sumy Ganas <aksmy.997@gmail.com>
2026-02-15 18:54:57 +0000

    remove redundant code

--- main.py
+++ main.py
@@ -8,53 +8,14 @@ from .strategy import main_strategy
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Triggered from a message on a Cloud Pub/Sub topic
-#Scheduled function to run every weekday at 9:30 am
-"""@functions_framework.cloud_event
-def subscribe(cloud_event: CloudEvent) -> (None):
-
-    print("Cloud function running!")
-    print(
-        "Pub/Sub message: " + base64.b64decode(cloud_event.data["message"]["data"]).decode()
-            )
-    if base64.b64decode(cloud_event.data["message"]["data"]).decode() == "buy":
-        print("Executing buy strat now!")
-        buy_instance = main_strategy.ClientInstance()
-        buy_instance.execute_buy_strategy()
-        logger.info("Buy Strategy complete!")
-        return None
-
-    if base64.b64decode(cloud_event.data["message"]["data"]).decode() == "sell":
-        print("Executing sell strat now!")
-        sell_instance = main_strategy.ClientInstance()
-        sell_instance.execute_sell_strategy()
-        logger.info("Sell strategy complete!")
-        return None
-
-    if base64.b64decode(cloud_event.data["message"]["data"]).decode() == "push":
-        print("Pushing data now!")
-        sell_instance = main_strategy.ClientInstance()
-        sell_instance.push_port_orders()
-        logger.info("Ports were pushed successfully!")
-        return None
-
-    #Testing the connection
-    print(
-        "TEST Pub/Sub message:" + base64.b64decode(cloud_event.data["message"]["data"]).decode()
-        )
-    return None"""
-
-# Triggered from a message on a Cloud Pub/Sub topic
-#Scheduled function to run every weekday at 9:30 am
 @functions_framework.cloud_event
 def subscribe(cloud_event: CloudEvent) -> (None):
     """
+    Triggered from a message on a Cloud Pub/Sub topic
+    Scheduled function to run every weekday at 9:30 am
     Recieves a scheduled cloud pub/sub event and is executed. Args: CloudEvent
     """
-    logger.info("Cloud function running!")
-
     msg = base64.b64decode(cloud_event.data["message"]["data"]).decode()
-    logger.info("Pub/Sub message: %s", msg)
 
     trading = main_strategy.ClientInstance()
 
